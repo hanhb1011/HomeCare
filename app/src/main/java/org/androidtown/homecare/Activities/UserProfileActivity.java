a2dac75f35ba790f399a03a5e3a7f7bcc996448f
1hanhb <1hanhb@naver.com>
2017-11-25 17:39:47 +0900

    user profile activity added

---
+++ app/src/main/java/org/androidtown/homecare/Activities/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package org.androidtown.homecare.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidtown.homecare.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
